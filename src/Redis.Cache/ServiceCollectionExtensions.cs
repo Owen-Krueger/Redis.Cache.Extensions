267e7a6d84e51cc21c75415bb272a199d172913d
Owen Krueger <37021716+Owen-Krueger@users.noreply.github.com>
2024-10-03 16:27:29 -0500

    v2.0.0 Changes (.NET 8.0, Unit Testing Package, ServiceCollection) (#2)

---
+++ src/Redis.Cache/ServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using StackExchange.Redis;
+
+namespace Redis.Cache;
+
+/// <summary>
+/// Extension methods for <see cref="ServiceCollection"/>.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// Adds a <see cref="RedisCache"/> to the service collection.
+    /// Inner <see cref="IDatabase"/> created automatically pointed at a localhost instance.
+    /// </summary>
+    public static void AddRedisCache(this ServiceCollection services) => 
+        services.AddTransient<IRedisCache, RedisCache>();
+    
+    /// <summary>
+    /// Adds a <see cref="RedisCache"/> to the service collection.
+    /// Inner <see cref="IDatabase"/> created automatically pointed at a specified host instance.
+    /// </summary>
+    public static void AddRedisCache(this ServiceCollection services, string host) => 
+        services.AddTransient<IRedisCache>(_ => new RedisCache(host));
+
+    /// <summary>
+    /// Adds a <see cref="RedisCache"/> to the service collection.
+    /// Inner <see cref="IDatabase"/> created with provided options.
+    /// </summary>
+    public static void AddRedisCache(this ServiceCollection services, ConfigurationOptions options) =>
+        services.AddTransient<IRedisCache>(_ => new RedisCache(options));
+    
+    /// <summary>
+    /// Adds a <see cref="RedisCache"/> to the service collection.
+    /// Inner <see cref="IDatabase"/> provided as a parameter.
+    /// </summary>
+    public static void AddRedisCache(this ServiceCollection services, IDatabase database) =>
+        services.AddTransient<IRedisCache>(_ => new RedisCache(database));
+}
